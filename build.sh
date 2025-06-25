dfb7c7e849a93ac6a6bb601b6f1e237703c2ca64
Maman Sani Ibrahim <mamansani.ibrahim01@gmail.com>
2025-06-25 17:46:50 +0100

    update build.sh to install the required tools

--- build.sh
+++ build.sh
@@ -8,7 +8,9 @@ go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
 go install -v github.com/projectdiscovery/dnsx/cmd/dnsx@latest
 
+# shellcheck disable=SC2016
 echo 'export PATH=$PATH:$(go env GOPATH)/bin' >> ~/.bashrc
+# shellcheck disable=SC1090
 source ~/.bashrc
 
 echo "[+] Build complete."
