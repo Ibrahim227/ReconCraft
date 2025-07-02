d0833e453ff25679c39f5897a0d1ad1bed8370cb
Maman Sani Ibrahim <mamansani.ibrahim01@gmail.com>
2025-07-02 12:49:38 +0100

    update build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
+set -e
 
 echo "[+] Installing Golang and recon tools..."
 
 apt-get update && apt-get install -y golang curl git
 
-go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
-go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
-go install -v github.com/projectdiscovery/dnsx/cmd/dnsx@latest
+export GOPATH="/go"
+export PATH="$PATH:$GOPATH/bin"
 
-# shellcheck disable=SC2016
-echo 'export PATH=$PATH:$(go env GOPATH)/bin' >> ~/.bashrc
-# shellcheck disable=SC1090
-source ~/.bashrc
+mkdir -p "$GOPATH/bin"
 
-echo "[+] Build complete."
+# Install tools
+go install github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest || true
+go install github.com/projectdiscovery/httpx/cmd/httpx@latest || true
+go install github.com/projectdiscovery/dnsx/cmd/dnsx@latest || true
+
+# Copy binaries to system PATH
+cp "$GOPATH/bin/"* /usr/local/bin/
+
+echo "[+] Go tools installed to /usr/local/bin"
