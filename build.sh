7bbae3b2de12dfe34974449fdd50cf378214864b
Maman Sani Ibrahim <mamansani.ibrahim01@gmail.com>
2025-07-02 13:00:23 +0100

    update render.yaml

--- build.sh
+++ build.sh
@@ -12,8 +12,19 @@ mkdir -p "$GOPATH/bin"
 
 # Install tools
 go install github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest || true
+go install github.com/tomnomnom/assetfinder@latest || true
 go install github.com/projectdiscovery/httpx/cmd/httpx@latest || true
 go install github.com/projectdiscovery/dnsx/cmd/dnsx@latest || true
+go install github.com/tomnomnom/waybackurls@latest || true
+go install github.com/lc/gau/v2/cmd/gau@latest || true
+go install github.com/tomnomnom/unfurl@latest || true
+go install github.com/tomnomnom/qsreplace@latest || true
+go install github.com/hakluke/hakrawler@latest || true
+go install github.com/projectdiscovery/katana/cmd/katana@latest || true
+go install github.com/jaeles-project/gospider@latest || true
+git clone https://github.com/projectdiscovery/alterx.git && cd alterx/v2/cmd/alterx && go install . || true
+
+
 
 # Copy binaries to system PATH
 cp "$GOPATH/bin/"* /usr/local/bin/
