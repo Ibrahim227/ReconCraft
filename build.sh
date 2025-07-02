fa442e6a9bc47f08c863c5258ccf848305d28aa4
Maman Sani Ibrahim <mamansani.ibrahim01@gmail.com>
2025-07-02 17:41:54 +0100

    update build.sh: remove some tools

--- build.sh
+++ build.sh
@@ -11,21 +11,15 @@ export PATH="$PATH:$GOPATH/bin"
 mkdir -p "$GOPATH/bin"
 
 # Install tools
-go install github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest || true
-go install github.com/tomnomnom/assetfinder@latest || true
-go install github.com/projectdiscovery/httpx/cmd/httpx@latest || true
-go install github.com/projectdiscovery/dnsx/cmd/dnsx@latest || true
-go install github.com/tomnomnom/waybackurls@latest || true
-go install github.com/lc/gau/v2/cmd/gau@latest || true
-go install github.com/tomnomnom/unfurl@latest || true
-go install github.com/tomnomnom/qsreplace@latest || true
-go install github.com/hakluke/hakrawler@latest || true
-go install github.com/projectdiscovery/katana/cmd/katana@latest || true
-go install github.com/jaeles-project/gospider@latest || true
-# shellcheck disable=SC2015
-git clone https://github.com/projectdiscovery/alterx.git && cd alterx/v2/cmd/alterx && go install . || true
-
-
+go install github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+go install github.com/tomnomnom/assetfinder@latest
+go install github.com/projectdiscovery/alterx/cmd/alterx@latest
+go install github.com/projectdiscovery/dnsx/cmd/dnsx@latest
+go install github.com/projectdiscovery/httpx/cmd/httpx@latest
+go install github.com/tomnomnom/waybackurls@latest
+go install github.com/lc/gau/v2/cmd/gau@latest
+go install github.com/tomnomnom/qsreplace@latest
+go install github.com/projectdiscovery/katana/cmd/katana@latest
 
 # Copy binaries to system PATH
 cp "$GOPATH/bin/"* /usr/local/bin/
